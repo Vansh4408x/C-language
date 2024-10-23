d66d5af331c77d7194380afdd1990e843167d006
Vansh4408x <bhumigupta7205@gmail.com>
2024-10-23 21:37:33 +0530

    Add files via upload

---
+++ Helloworld.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main() {
+    printf("Hello world
+   printf("Hello world!");
+   printf("Hello world!");
+   printf("Hello world!");
+   printf("Hello world!");
+   printf("Hello world!");
+   printf("Hello world!");
+   printf("Hello world!");
+    
+    printf("hello vansh");
+    
+printf("Hello world!");
+    return 0;
+  
+    #include<stdio.h>
+    
+    int main() {
+printf("Hello world!");
+   printf("Hello world!");
+   printf("Hello c");
+   printf("Hello nandini");
+    return 0;
+    }
